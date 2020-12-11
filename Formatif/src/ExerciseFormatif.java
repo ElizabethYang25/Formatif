a7c747b758710a04f9a5f37eb43256a8cbe60d82
elizabethyang <elizabethyang@l-y>
2020-12-11 13:53:19 -0500

    11 Décembre 2020

---
+++ Formatif/src/ExerciseFormatif.java
@@ -0,0 +1,14 @@
+/*
+* Author : elizabethyang
+*Date : 11-Dec-2020
+*/
+public class ExerciseFormatif
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+	}
